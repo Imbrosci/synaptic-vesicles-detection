643c7708aacfd07ef755c4cd155a47928ae1ff51
Barbara Imbrosci <42861552+Imbrosci@users.noreply.github.com>
2021-06-27 23:48:34 +0200

    Add files via upload

---
+++ dataset_modifications.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 28 12:06:59 2020
+
+@author: barbara
+"""
+import torch
+
+class AddGaussianNoise(object):
+    def __init__(self,mean=0.,std=1.):
+        self.mean=mean
+        self.std=std
+    
+    def __call__(self,tensor):
+        return tensor + torch.randn(tensor.size())*self.std+self.mean
+    
+    def __repr__(self):
+        return self.__class__.name__ + '(mean={0}, std{1})'.format(self.mean,self.std)     
+        
